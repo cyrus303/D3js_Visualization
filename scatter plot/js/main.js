99d3d623cbf781178ac5a77022b1a779114d38dd
Sachin Mahesh <31949367+cyrus303@users.noreply.github.com>
2019-11-16 18:11:20 +0000

    scatter plot using d3js
    scatter plot switching between two different columns from the data set

---
+++ scatter plot/js/main.js
@@ -0,0 +1,167 @@
+var margin = { top:10, right:20, bottom: 50, left:80 }
+
+var width = 600 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
+var flag = true;
+var t = d3.transition().duration(750)
+
+var svg = d3.select("#chart-area")
+	.append("svg")
+		.attr("width",width + margin.left + margin.right)
+		.attr("height",height + margin.top + margin.bottom)
+var g = svg.append("g")
+	.attr("transform","translate("+margin.left+","+margin.top+")");
+
+var xAxisGroup = g.append("g")
+	.attr("class","bottom axis")
+	.attr("transform","translate(0, "+ (height) + ")")
+
+var yAxisGroup = g.append("g")
+	.attr("class","left axis");
+
+//X scale
+var x = d3.scaleBand()
+	.range([0,width])
+	.padding(0.2)//Inner(0.3)
+	//.paddingOuter(0.3);
+
+//Y scale
+var y = d3.scaleLinear()
+	.range([height,0])
+	
+
+			
+
+g.append("text")
+	.attr("class", "x axis-label")
+	.attr("x", (width/2))
+	.attr("y", (height + 40))
+	.attr("font-size","20px")
+	.attr("text-anchor","middle")
+	//.attr("color","black")
+	.text("month");
+
+var yLabel = g.append("text")
+	.attr("class", "y axis-label")
+	.attr("x", -(height/2))
+	.attr("y", (-60))
+	.attr("font-size","20px")
+	.attr("transform","rotate(-90)")
+	.attr("text-anchor","middle")
+	//.attr("color","black")
+	.text("revenue");
+
+//data load
+d3.json("data/revenues.json").then(function(data){
+	//console.log(data);
+	data.forEach(function(d){
+		d.revenue = +d.revenue;
+		d.profit = +d.profit;
+	});
+
+	d3.interval(function(){
+		var newdata = flag ? data : data.slice(1);
+
+		update(newdata)
+		flag = !flag
+	}, 1000);
+
+	update(data);
+
+});
+
+function update(data){
+
+	var value = flag ? "revenue" : "profit";
+	
+	x.domain(data.map(function(d){ return (d.month )}));
+	y.domain([0, d3.max(data, function(d){ return d[value]})])
+
+	//y axis
+	var leftAxis = d3.axisLeft(y)
+		//.ticks(5)
+		.tickFormat(function(d){return d + "$" });
+	yAxisGroup.transition(t).call(leftAxis);
+
+	//x axis
+	var bottomAxis = d3.axisBottom(x);
+	xAxisGroup.transition(t).call(bottomAxis);;
+		
+
+	//bars
+	
+	var rects = g.selectAll("circle")
+		.data(data, function(d){
+			return d.month;
+		});
+		//console.log(data);
+
+	//JOIN new data with old elements
+	//var rects = g.selectAll("rect")
+	//	.data(data, function(d){
+	//		return d.month;
+	//	});
+
+
+	//EXIT old elements not present in new data
+	rects.exit()
+		.attr("fill", "grey")
+	.transition(t)
+		.attr("cy",y(0))
+		
+		.remove();
+
+	//UPDATE old elements present in new data
+	/*rects.transition(t)
+		.attr("x",function(d,i){
+				return x(d.month);
+			})
+
+		.attr("y", function(d){
+				return y(d[value])
+			})
+
+		.attr("width",x.bandwidth)
+
+		.attr("height",function(d){
+				return height - y(d[value]);
+			})*/
+
+	// ENTER new element present in new data
+	rects.enter()
+		.append("circle")
+			.attr("cx",function(d){
+				return (x(d.month) + x.bandwidth() /2);
+			})
+			.attr("cy", y(0))
+			.attr("r",5)			
+
+			
+			.attr("fill","red")
+		//before merge enter method
+		.merge(rects)
+		//after merge update method
+		.transition(t)
+			.attr("cy", function(d){
+				return y(d[value])
+			})
+			.attr("cx",function(d){
+				return (x(d.month) + x.bandwidth() /2);
+			})
+			
+
+
+	var label = flag ? "Revenue":"Profit";
+	yLabel.text(label);
+}
+
+
+
+
+
+
+
+
+
+
